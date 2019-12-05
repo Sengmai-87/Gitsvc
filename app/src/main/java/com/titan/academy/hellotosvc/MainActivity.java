377bbe403c3968d1dcce0a36fe2f76db2bf59566
Sengmai-87 <58464945+Sengmai-87@users.noreply.github.com>
2019-12-05 15:33:23 +0630

    test2 commit

--- app/src/main/java/com/titan/academy/hellotosvc/MainActivity.java
+++ app/src/main/java/com/titan/academy/hellotosvc/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //test
+        //test 3
         setContentView(R.layout.activity_main);
     }
 }
